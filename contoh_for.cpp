651020c88c5494e4ebc48af33fd37c4a5e90f1c4
zaknisdev <zakianisf1710@gmail.com>
2025-03-17 12:04:47 +0700

    memasukan perulangan for

--- contoh_for.cpp
+++ contoh_for.cpp
@@ -9,7 +9,22 @@ int main()
     int perulanganWhile = 0;
     int perulanganDo = 0;
 
-    
+    // perulangan dengan for
 
-    
+    cout << "Perulangan for pencacahan naik" << endl;
+    for (n = 0; n<5; n++)
+    {
+        cout << "Nilai n =" << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir =" << n << endl;
+    cout << endl; 
+
+    cout << "Perulangan for pencacahan turun" << endl;
+    // perulangan dengan for
+    for (n = 5; n>0; n--)
+    {
+        cout << "Nilai n =" << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir =" << n << endl;
+    cout << endl; 
 }
