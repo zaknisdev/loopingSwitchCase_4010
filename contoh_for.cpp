130ed120d4cf0e4a32b6d10e1968dfdabd5ce346
zaknisdev <zakianisf1710@gmail.com>
2025-03-17 12:00:32 +0700

    Memasukan variable

---
+++ contoh_for.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{ 
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+    
+
+    
+}
